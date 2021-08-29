d80b01dca7b03128ed053f7964fd47484232731f
WillZhangFly <oliverottawa@gmail.com>
2021-08-29 01:29:31 -0400

    setup express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,20 @@
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
-import { Post } from './entities/Post';
 import mikroConfig from './mikro-orm.config';
+import express from 'express';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up();
+  
+  const app = express();
+  app.get('/', (_ , res) => {
+    res.send("Hello000");
+  });
+  app.listen(4000 , () => {
+    console.log('server started on localhost:4000');
+  })
 
-  const post = orm.em.create(Post, { title: 'my first post' });
-  await orm.em.persistAndFlush(post);
-
-  const posts = await orm.em.find(Post , {});
-  console.log(posts);
 };
 
 main().catch((err) => {
