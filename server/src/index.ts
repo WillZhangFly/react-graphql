00f7732f8d0c737414105b1a0c80aeaeefc3854b
WillZhangFly <oliverottawa@gmail.com>
2021-08-28 20:50:34 -0400

    first post of postgresql

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,10 @@
 import { MikroORM} from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
+import mikroConfig from "./mikro-orm.config";
 
 const main = async () => {
-   const orm = await MikroORM.init({
-       entities:[Post],
-       dbName:"lireddit",
-       type:"postgresql",
-       debug: !__prod__,
-       user:"postgres",
-       password:"Sql6533!"
-   });
+   const orm = await MikroORM.init(mikroConfig);
 
    const post = orm.em.create(Post , {title:"my first post"});
 
