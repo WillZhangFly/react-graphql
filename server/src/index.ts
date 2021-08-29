9b32070f3393825891f232319e87b7ad1cacd868
WillZhangFly <oliverottawa@gmail.com>
2021-08-29 01:14:44 -0400

    mikro orm basic setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,19 @@
-import { MikroORM} from "@mikro-orm/core";
-import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
-import mikroConfig from "./mikro-orm.config";
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+import mikroConfig from './mikro-orm.config';
 
 const main = async () => {
-   const orm = await MikroORM.init(mikroConfig);
+  const orm = await MikroORM.init(mikroConfig);
+  await orm.getMigrator().up();
 
-   const post = orm.em.create(Post , {title:"my first post"});
+  const post = orm.em.create(Post, { title: 'my first post' });
+  await orm.em.persistAndFlush(post);
 
-   await orm.em.persistAndFlush(post);
-   await orm.em.nativeInsert(Post , {title :'my first post 2'});
+  const posts = await orm.em.find(Post , {});
+  console.log(posts);
 };
 
-main().catch(err => {
-    console.log(err)
+main().catch((err) => {
+  console.log(err);
 });
-
