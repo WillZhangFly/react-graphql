24105f3b13fd525f980a33c1155dea9e153510da
WillZhangFly <oliverottawa@gmail.com>
2021-08-28 20:19:17 -0400

    mikro orm and postgres initial setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,24 @@
-console.log("abc");
+import { MikroORM} from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+
+const main = async () => {
+   const orm = await MikroORM.init({
+       entities:[Post],
+       dbName:"lireddit",
+       type:"postgresql",
+       debug: !__prod__,
+       user:"postgres",
+       password:"Sql6533!"
+   });
+
+   const post = orm.em.create(Post , {title:"my first post"});
+
+   await orm.em.persistAndFlush(post);
+   await orm.em.nativeInsert(Post , {title :'my first post 2'});
+};
+
+main().catch(err => {
+    console.log(err)
+});
+
