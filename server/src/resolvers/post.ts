a159027178a2986702948faaa28ce8e0f407a09c
WillZhangFly <oliverottawa@gmail.com>
2021-08-29 14:11:40 -0400

    get post info by id

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,6 +1,6 @@
 import { Post } from "../entities/Post";
 import { MyContext } from "../types";
-import { Ctx, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Query, Resolver, Int } from "type-graphql";
 
 @Resolver()
 export class PostResolver {
@@ -9,5 +9,13 @@ export class PostResolver {
         @Ctx() {em}:MyContext
     ): Promise<Post[]>{
         return em.find(Post, {});
+    }
+
+    @Query(() => Post, {nullable : true})
+    post(
+        @Arg("id" , () => Int) id: number,
+        @Ctx() {em}:MyContext
+    ): Promise<Post | null>{
+        return em.findOne(Post, {id});
     }
 }
