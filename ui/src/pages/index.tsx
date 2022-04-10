23a2093677ab4dc088ce601d9e7791dd34bf230b
WillZhangFly <oliverottawa@gmail.com>
2022-04-10 18:32:41 -0400

    ssr setup 2

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -1,5 +1,22 @@
-import Nav from '../components/Nav';
+import { withUrqlClient } from "next-urql";
+import Nav from "../components/Nav";
+import { createUrqlClient } from "../utils/createUrqlClient";
+import { usePostsQuery } from "../generated/graphql";
 
-const Index = () => <Nav></Nav>;
+const Index = () => {
+  const [{ data }] = usePostsQuery();
+  return (
+    <>
+      <Nav></Nav>
+      <div>Hello World!!!</div>
+      <div>--------------</div>
+      {!data ? (
+        <div>Loading</div>
+      ) : (
+        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+      )}
+    </>
+  );
+};
 
-export default Index;
+export default withUrqlClient(createUrqlClient, {ssr : true})(Index);
